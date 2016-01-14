00059feea0625f08a09575ab640f0eb81d28d424
Bodie Solomon <bodie@synapsegarden.net>
2016-01-14 16:44:05 -0500

    Move source text to const

--- api/source.go
+++ api/source.go
@@ -9,6 +9,15 @@ import (
 	"github.com/synapse-garden/mf-proto/db"
 )
 
+const source = `---     S Y N A P S E G A R D E N      ---
+            MF-Proto v0.3.0
+         © SynapseGarden 2015
+ Licensed under Affero GNU Public License
+                version 3
+https://github.com/synapse-garden/mf-proto
+---                                    ---
+`
+
 func Source(d db.DB) API {
 	return func(r *htr.Router) error {
 		r.GET("/source", handleSource)
@@ -17,12 +26,5 @@ func Source(d db.DB) API {
 }
 
 func handleSource(w http.ResponseWriter, r *http.Request, ps htr.Params) {
-	fmt.Fprint(w, `---     S Y N A P S E G A R D E N      ---
-            MF-Proto v0.3.0  
-         © SynapseGarden 2015
- Licensed under Affero GNU Public License
-                version 3
-https://github.com/synapse-garden/mf-proto
----                                    ---
-`)
+	fmt.Fprint(w, source)
 }
